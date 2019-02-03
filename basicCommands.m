d91cafaa712dc5f89d728042ad1c3acd64a0ee42
ImDaNoob <bjchappell02@icloud.com>
2019-02-03 16:35:25 -0500

    First Commit

---
+++ basicCommands.m
@@ -0,0 +1,36 @@
+## Benjamin Chappell ##
+
+5+6
+
+3-2
+
+1/2
+
+2^6
+
+1==2 %false 
+
+1~=2 %true
+
+1 && 0
+
+1 || 0
+
+xor(1, 0)
+
+%semicolon makes it so that you don't print out whatever happens
+
+a = 3
+a=3;
+
+A = [1 2; 3 4 ; 5 6]
+
+v = 1:0.1:2
+
+ones(2, 3)
+
+C = 2*ones(2,3)
+
+w = randn(2, 6)
+
+hist(w)
